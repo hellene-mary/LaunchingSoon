eb01272a2ed155fb5659e2def079c528729b50ba
Glitch (launchingsoon) <none>
2024-02-06 11:38:57 +0000

    🪰🩴 Updated with Glitch

--- index.js
+++ index.js
@@ -1,36 +1,47 @@
-
 // дата відкриття
-const openingDate = new Date('2024-10-12 10:00:00');
+const openingDate = new Date("2024-04-01 10:00:00");
 
 // реф
 // const stopBtn = document.querySelector(".btnStop");
-const dayValueEl = document.querySelector('[data-days]');
-const hoursValueEl = document.querySelector('[data-hours]');
-const minValueEl = document.querySelector('[data-minutes]');
-const secValueEl = document.querySelector('[data-seconds]');
+const dayFirstValueEl = document.querySelector("[data-days-fst]");
+const daySecondValueEl = document.querySelector("[data-days-snd]");
+const hoursFirstValueEl = document.querySelector("[data-hours-fst]");
+const hoursSecondValueEl = document.querySelector("[data-hours-snd]");
+const minFirstValueEl = document.querySelector("[data-minutes-fst]");
+const minSecondValueEl = document.querySelector("[data-minutes-snd]");
+// const secValueEl = document.querySelector('[data-seconds]');
+const secFirstValueEl = document.querySelector("[data-seconds-fst]");
+const secSecondValueEl = document.querySelector("[data-seconds-snd]");
 
 let timerId = null;
 
 // функція підрухунку часу до відкриття
-function timer () {
+function timer() {
   timerId = setInterval(() => {
     if (openingDate.getTime() >= Date.now()) {
       const timerIndicator = openingDate.getTime() - Date.now();
       const { days, hours, minutes, seconds } = convertMs(timerIndicator);
       // console.log('~ Time for end', `${days}:${hours}:${minutes}:${seconds}`);
-      
-      dayValueEl.textContent = days;
-      hoursValueEl.textContent = hours;
-      minValueEl.textContent = minutes;
-      secValueEl.textContent = seconds;
+
+      dayFirstValueEl.textContent = days.split("")[0];
+      daySecondValueEl.textContent = days.split("")[1];
+
+      hoursFirstValueEl.textContent = hours.split("")[0];
+      hoursSecondValueEl.textContent = hours.split("")[1];
+
+      minFirstValueEl.textContent = minutes.split("")[0];
+      minSecondValueEl.textContent = minutes.split("")[1];
+
+      secFirstValueEl.textContent = seconds.split("")[0];
+      secSecondValueEl.textContent = seconds.split("")[1];
     } else {
-      alert("The opening date has already passed!")
+      alert("The opening date has already passed!");
     }
   }, 1000);
 }
 
 function addLeadingZero(value) {
-  return String(value).padStart(2, '0');
+  return String(value).padStart(2, "0");
 }
 
 function convertMs(ms) {
@@ -38,22 +49,22 @@ function convertMs(ms) {
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
+
   const days = addLeadingZero(Math.floor(ms / day));
   const hours = addLeadingZero(Math.floor((ms % day) / hour));
   const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
-  const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
+  const seconds = addLeadingZero(
+    Math.floor((((ms % day) % hour) % minute) / second)
+  );
 
   return { days, hours, minutes, seconds };
 }
 
 // зупинка таймеру
-function onStopBtnClick () {
+function onStopBtnClick() {
   console.log("stop");
   clearInterval(timerId);
-};
+}
 
 // stopBtn.addEventListener("click", onStopBtnClick);
-timer()
-
-
-
+timer();
